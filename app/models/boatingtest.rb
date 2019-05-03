f96013ab3d5e9436c5cf35189fe74ceb345cd47d
Andrew Allen <asallen67@gmail.com>
2019-05-03 12:50:36 -0400

    done

--- app/models/boatingtest.rb
+++ app/models/boatingtest.rb
@@ -1,3 +1,21 @@
 class BoatingTest
+	attr_accessor :student, :name, :status, :instructor
+	@@all = []
 
+	def self.all
+		@@all
+	end
+
+	def initialize(student, name, status="pending", instructor)
+		@student = student
+		@name = name
+		@status = status
+		@instructor = instructor
+		
+		@@all << self
+	end
+
+	def self.find_by_name(test_name)
+		@@all.select {|test| test.name.downcase == test_name.downcase}
+	end
 end
